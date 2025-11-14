04371dcf7b6525523f03120642f58fae88013d33
amin dehnavi <amin1384dehnavi@gmail.com>
2025-11-14 22:10:06 +0330

    🔧fix(layout): convert to jsx

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+//* CSS Imports
+import "./globals.css";
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+//* React | Next Imports
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
