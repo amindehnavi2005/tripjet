3d46713b0467c010af16bcc9e9fdc80a88c7aead
amin dehnavi <amin1384dehnavi@gmail.com>
2025-11-15 22:06:41 +0330

    ➕feat(outside-tours): create new page

---
+++ src/app/outside-tours/page.jsx
@@ -0,0 +1,37 @@
+//* React | Next Imports
+import React from 'react'
+import Image from 'next/image'
+//* Component Imports
+import { Header } from '@/components/layout/common/Header'
+import { LandingSearchBox } from '@/components/layout/landing/LandingSearchBox'
+
+const page = () => {
+    return (
+        <div>
+            <Header />
+            <LandingSearchBox isTourShow={false} />
+            <div className='flex items-center mt-20 mx-6 md:mx-28 gap-6 mb-10'>
+                <div className='flex-1'>
+                    <p className='shadow p-2 rounded-lg'>تعداد نتایج : 169</p>
+                </div>
+                <div className='flex-[4]'>
+                    <div className='grid grid-cols-3 gap-6'>
+                        <Image src={"/images/Ticket.png"} height={43} width={317} alt='تریپ جت' className='w-full' />
+                        <div className='flex flex-col gap-4'>
+                            <p className='font-bold text-xl'>تور ترکیبی بدروم ازمیر</p>
+                            <p className='text-gray-600'>۱۴۰۳/۰۸/۰۴</p>
+                            <p className='text-gray-600'>۶ شب و ۷ روز</p>
+                            <p className='text-gray-600'>هتل ۵ ستاره</p>
+                        </div>
+                        <div className='flex flex-col gap-5 text-center'>
+                            <p className='text-2xl font-bold'>45,250,000</p>
+                            <button className='bg-blue-600 text-white p-3 rounded-xl'>مشاهده جزئیات و رزرو</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
--- src/components/layout/landing/LandingSearchBox.jsx
+++ src/components/layout/landing/LandingSearchBox.jsx
@@ -2,15 +2,15 @@
 import React from 'react'
 import Image from 'next/image'
 
-export const LandingSearchBox = () => {
+export const LandingSearchBox = ({ isTourShow = true }) => {
     return (
         <div className='relative'>
             <Image src={"/images/LandingBanner.png"} height={43} width={800} alt='تریپ جت' className='w-full' />
             <div className='absolute -bottom-10 border-2 rounded-xl md:right-[25%] right-0 md:mx-28 mx-6 p-6 grid gap-14 items-center bg-white'>
-                <div className='flex items-center gap-8'>
+                {isTourShow && <div className='flex items-center gap-8'>
                     <p>تور های خارجی</p>
                     <p>تور های داخلی</p>
-                </div>
+                </div>}
                 <div className='flex items-center gap-8'>
                     <input placeholder='مبدا' />
                     <input placeholder='مقصد' />
